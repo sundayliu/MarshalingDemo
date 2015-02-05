36155b0a03291800d06d09afff7d19ec4ae14542
sundayliu <sundayliu@aliyun.com>
2015-02-05 12:54:42 +0800

    CMake Hello

---
+++ Test/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class InitInfo
+    {
+        public int size;
+        public int game_id;
+        public IntPtr send_data_to_svr;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.WriteLine("IntPtr Size:{0}", IntPtr.Size);
+
+            InitInfo init_info = new InitInfo();
+            Console.WriteLine("{0}", Marshal.SizeOf(init_info));
+        }
+    }
+}
